b22ce07ff5459aaa10dfc70d6ee15cea921e256e
smaru1111 <marusou20@gmail.com>
2023-08-14 17:56:06 +0900

    #25 update Work型に職種カテゴリを追加

--- find-job/src/app/lib/contents.ts
+++ find-job/src/app/lib/contents.ts
@@ -6,16 +6,30 @@
 //   place: string
 //   requirement:string
 // }
+const enum enumCategory {
+  "教育",
+  "軽作業",
+  "配達・運転",
+  "販売・接客",
+  "オフィスワーク",
+  "イベント・キャンペーン",
+  "専門職",
+  "エンタメ",
+  "アート・デザイン",
+  "その他"
+}
+
 
 export type Work = {
     [x: string]: any;
-    timestamp: number;
-    name: string;
-    Elements: string;
-    salary: number;
-    time: string;
-    people: string;
-    place: string;
-    require: string;
+    timestamp: number; // タイムスタンプ,主キー
+    name: string; // 
+    Elements: string; // 仕事内容
+    // workCategoryをenumで定義する
+    workCategory: enumCategory; // 仕事の種類
+    salary: number; // 給与
+    time: string; // 勤務時間
+    people: string; // 募集人数
+    place: string; // 勤務地
+    require: string; // 必要なスキル
   };
-
