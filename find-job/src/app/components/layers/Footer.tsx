0988bc5b8e479a61ffcec2d037b0752611c5955d
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-08-17 17:01:43 +0900

    #28 update フッターヘッダーのスタイル調整

--- find-job/src/app/components/layers/Footer.tsx
+++ find-job/src/app/components/layers/Footer.tsx
@@ -4,18 +4,20 @@ import Link from 'next/link';
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className="flex flex-col bg-primary pt-8 px-8 pb-3 mt-10">
-        <div className="flex justify-between">
-          <Link href='/' className=" text-white hover:opacity-60 transition-opacity duration-500 ease-out">バイトの森</Link>
-          <div className=' grid grid-cols-2 gap-4'>
-            <Link href='/' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>仕事を探す</Link>
-            <Link href='/about' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>このサイトについて</Link>
-            <Link href='/' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>アンケート</Link>
+      <footer className=" bg-primary">
+        <div className="flex flex-col text-white text-sm container md:px-14 px-8 pt-8 pb-3 mt-10 mb-0 mx-auto max-w-5xl">
+          <div className="flex justify-between">
+            <Link href='/' className=" text-white hover:opacity-60 transition-opacity duration-500 ease-out">バイトの森</Link>
+            <div className=' grid grid-cols-2 gap-4'>
+              <Link href='/' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>仕事を探す</Link>
+              <Link href='/about' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>このサイトについて</Link>
+              {/* <Link href='/' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>アンケート</Link> */}
+            </div>
           </div>
+          <p className=" text-white text-[10px] text-center mt-14">
+            © abcdef.All rights reserved.
+          </p>
         </div>
-        <p className=" text-white text-[10px] text-center mt-14">
-          © abcdef.All rights reserved.
-        </p>
       </footer>
     </>
   );
