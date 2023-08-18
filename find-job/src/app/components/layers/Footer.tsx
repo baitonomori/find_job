5ced4132547c3d0505a6ab2cec0c794eba1ea379
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-08-19 00:22:18 +0900

    #28 update about画面の調整

--- find-job/src/app/components/layers/Footer.tsx
+++ find-job/src/app/components/layers/Footer.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className=" bg-primary">
-        <div className="flex flex-col text-white text-sm container md:px-14 px-5 pt-8 pb-3 mt-10 mb-0 mx-auto max-w-5xl">
+      <footer className=" bg-primary mt-20">
+        <div className="flex flex-col text-white text-xs container md:px-14 px-5 pt-8 pb-3 mt-6 mb-0 mx-auto max-w-5xl">
           <div className="flex justify-between">
             <Link href='/' className=" text-white hover:opacity-60 transition-opacity duration-500 ease-out">バイトの森</Link>
             <div className='flex'>
-              <Link href='/' className='text-white text-xs align-middle md:mr-8 mr-4 hover:opacity-60 transition-opacity duration-500 ease-out'>仕事を探す</Link>
-              <Link href='/about' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>このサイトについて</Link>
+              <Link href='/' className='text-white align-middle md:mr-8 mr-4 hover:opacity-60 transition-opacity duration-500 ease-out'>仕事を探す</Link>
+              <Link href='/about' className='text-white align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>このサイトについて</Link>
               {/* <Link href='/' className='text-white text-xs align-middle hover:opacity-60 transition-opacity duration-500 ease-out'>アンケート</Link> */}
             </div>
           </div>
-          <p className=" text-white text-[10px] text-center mt-14">
+          <p className=" text-white text-[10px] text-center md:mt-28 mt-20">
             © abcdef.All rights reserved.
           </p>
         </div>
--- find-job/src/app/components/modal/CommonModal/Panel.tsx
+++ find-job/src/app/components/modal/CommonModal/Panel.tsx
@@ -18,7 +18,7 @@ const Panel: React.FC<Props> = props => {
     <section className=" z-50 relative w-full md:min-w-[675px] min-w-[80vw] md:max-w-5xl">
       <div className=" md:px-14 px-5">
         <WorkDetail content={props.content}/>
-        <button onClick={props.close} className=" [&>*]:pointer-events-none  z-[51] absolute md:right-[30px] right-[6px] top-[10px] rounded-[50%] p-2 bg-primary">
+        <button onClick={props.close} className=" [&>*]:pointer-events-none  z-[51] absolute md:right-[43px] right-[6px] top-[10px] rounded-[50%] p-2 bg-primary">
           <IconContext.Provider value={{ color: "white", size: "2em" }}>
             <IoCloseOutline />
           </IconContext.Provider>
