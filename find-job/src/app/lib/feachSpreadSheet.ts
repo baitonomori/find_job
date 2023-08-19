3e9d735f140153380f7e890410f2a4fd5fe521c2
nnnnn81 <nnnnnagi81@gmail.com>
2023-08-20 03:13:47 +0900

    update: 全表示できるようにした

--- find-job/src/app/lib/feachSpreadSheet.ts
+++ find-job/src/app/lib/feachSpreadSheet.ts
@@ -10,7 +10,35 @@ export const getAllData = async(target:string) => {
     }
     const data = await res.json();
     const filteredData = data.filter((item: any[])=> item[3] === target);
-    return filteredData;
+    if(data && target == "その他"){
+      const formatedData = filteredData.map((item: any[]) => ({
+        timestamp: item[0],
+        name: item[13],
+        Elements: item[12],
+        workCategory: item[14],
+        salary: item[15],
+        time: item[16],
+        people: item[17],
+        place: item[18],
+        require: item[19]
+      }))
+      return formatedData;
+    }
+    if(data && target == "家庭教師"){
+      const formatedData = filteredData.map((item: any[]) => ({
+        timestamp: item[0],
+        place: item[4],
+        time: item[5],
+        studentGender: item[6],
+        teacherGender: item[7],
+        studentGrade: item[8],
+        subject: item[9],
+        salary: item[10],
+        require: item[11]
+      }))
+      return formatedData;
+    }
+    return [];
   } catch (error) {
     console.error("[error]", error);
   }
@@ -35,7 +63,18 @@ export const getSearchResult = async(select:string, min:number, max:number, cate
       throw new Error("fetch失敗");
     }
     const data = await res.json();
-    return data;
+    const formatedData = data.map((item: any[]) => ({
+      timestamp: item[0],
+      name: item[13],
+      Elements: item[12],
+      workCategory: item[14],
+      salary: item[15],
+      time: item[16],
+      people: item[17],
+      place: item[18],
+      require: item[19]
+    }))
+    return formatedData;
   } catch (error) {
     console.error("[error]", error);
   }
